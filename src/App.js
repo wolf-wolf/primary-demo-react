00e2de77d7e9294d11006a04b0858f40ecbfc247
hunter.li <liming_pr@163.com>
2019-09-15 19:02:23 +0800

    temperature calculator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Game from "./Game";
+import TemperatureCalculator from "./pages/TemperatureCalculator/TemperatureCalculator";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Game />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Game/>
+                <TemperatureCalculator/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
